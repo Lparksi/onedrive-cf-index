fa0008a6fd8cc94409f170a0d8082f3bd28b560d
Lparksi <60086490+Lparksi@users.noreply.github.com>
2021-07-02 19:28:11 +0800

    Update htmlWrapper.js

--- src/render/htmlWrapper.js
+++ src/render/htmlWrapper.js
@@ -32,6 +32,7 @@ export function renderHTML(body, pLink, pIdx) {
       <meta charset="utf-8" />
       <meta http-equiv="x-ua-compatible" content="ie=edge, chrome=1" />
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+      <script data-ad-client="ca-pub-9917066068847885" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       <title>Parksi's OneDrive</title>
       <link rel="shortcut icon" type="image/png" sizes="16x16" href="${favicon}" />
       <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.13.1/css/all.min.css" rel="stylesheet">
